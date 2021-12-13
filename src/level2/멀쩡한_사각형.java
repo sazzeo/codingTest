fca089a3d2cf1c594b9450b4acb29b265fde8c6c
sazzeo <imjee02@gmail.com>
2021-12-13 20:39:16 +0900

    [지영] 코테

---
+++ src/level2/멀쩡한_사각형.java
@@ -0,0 +1,32 @@
+package level2;
+
+public class 멀쩡한_사각형 {
+	
+	
+	public static void main(String[] args) {
+		
+		int w = 8 , h = 12;
+		
+		System.out.println(solution(w,h));
+		
+		
+	}
+	
+	
+    public static long solution(int w, int h) {
+    	
+    	//1. 최대공약수로 나눈다.
+    	
+    	
+    	
+    	
+        return 1;
+        
+        
+    }
+    
+    
+    
+    
+
+}
