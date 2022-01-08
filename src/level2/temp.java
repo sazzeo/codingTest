b2b536211ca1d0b20b41ad8cf6b74d0798e422ae
sazzeo <imjee02@gmail.com>
2022-01-08 22:26:58 +0900

    코테

--- src/level2/temp.java
+++ src/level2/temp.java
@@ -8,18 +8,11 @@ public class temp {
 	
 	public static void main(String[] args) {
 		
-		
-		List<String> list = new ArrayList<>();
-		list.add("1");
-		list.add("2");
-		list.add("3");
-		
-		System.out.println("원래 List:" + list.toString());
-
-		List<String> copy = new ArrayList<>(list);
-		copy.remove(0);
-		System.out.println(copy.toString());
-		System.out.println(list.toString());
+		if(1<=2) {
+			
+			System.out.println("연산자 실험");
+			
+		}
 	}
 
 }
