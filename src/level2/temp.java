9153230c9cb156f67f8fb6ade6c217214f369afc
sazzeo <imjee02@gmail.com>
2022-01-13 00:10:48 +0900

    ㅋㅌ

--- src/level2/temp.java
+++ src/level2/temp.java
@@ -8,10 +8,12 @@ public class temp {
 	
 	public static void main(String[] args) {
 		
-		if(1<=2) {
+		try {
+			System.out.println("에러테스트");
+			throw new Exception();
 			
-			System.out.println("연산자 실험");
-			
+		} catch (Exception e) {
+			System.out.println("에러 발생");
 		}
 	}
 
