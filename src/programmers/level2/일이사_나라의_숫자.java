5c41f99e915652df4f46c54fc66b0e7e96c2f9c7
sazzeo <imjee02@gmail.com>
2022-05-27 14:23:58 +0900

    일이사 나라의 숫자

---
+++ src/programmers/level2/일이사_나라의_숫자.java
@@ -0,0 +1,48 @@
+package programmers.level2;
+
+public class 일이사_나라의_숫자 {
+
+	public static void main(String[] args) {
+
+		int n = 21;
+		solution(n);
+
+	}
+
+	/*
+	 * 1 1 6 14 2 2 7 21 3 4 8 22 4 11 9 24 5 12 10 41
+	 * 
+	 */
+
+	// 3진수로 변환 : 해당숫자에 0이 있으면 위 10을 아래 4로 변환 =>0이 없어질떄까지 반복
+	public static String solution(int n) {
+
+		StringBuilder res = new StringBuilder("");
+		while (n != 0) {
+			res.append(n % 3);
+			n /= 3;
+		}
+		String num = res.reverse().toString();
+
+		while (num.contains("0")) {
+
+			if (num.contains("40")) {
+				num = num.replace("40", "24");
+			}
+			if (num.contains("20")) {
+				num = num.replace("20", "14");
+			}
+			if (num.contains("10")) {
+				num = num.replace("10", "04");
+			}
+			if (num.charAt(0) == '0') {
+				num = num.substring(1);
+			}
+
+
+		}
+
+		return num;
+	}
+
+}
