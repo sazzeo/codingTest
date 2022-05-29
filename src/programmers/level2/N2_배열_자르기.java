adee50e613d14d6c78423d87fbb2fc0552140450
sazzeo <imjee02@gmail.com>
2022-05-29 22:14:27 +0900

    n^2 배열 자르기

---
+++ src/programmers/level2/N2_배열_자르기.java
@@ -0,0 +1,42 @@
+package programmers.level2;
+
+import java.util.Arrays;
+
+public class N2_배열_자르기 {
+	
+	public static void main(String[] args) {
+		
+		int n = 4;
+		long left = 7;
+		long right = 14;
+		
+		solution(4,0,14);
+		
+		
+	}
+
+	
+    public static int[] solution(int n, long left, long right) {
+    	
+        int[] answer = new int[(int)(right-left)+1];
+
+		   
+        for(long i=  left ; i<= right ; i++ ) {
+        	
+        	long row = i/n;
+        	long col = i%n;
+        	if(col<=row) {
+        		answer[(int)(i-left)] = (int)row+1;
+        		
+        		
+        	}else {
+        		answer[(int)(i-left)] = (int)col+1;        	}
+        	
+        }
+        
+
+        
+        
+        return answer;
+    }
+}
